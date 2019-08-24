d07a546ebfb49fbfb7e7a2cc19cc1b0445b20952
Daniele Vinci <vinci.dani@gmail.com>
2019-08-25 01:57:43 +0200

    header and sidedrawer components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './resources/styles.css';
+import Header from './components/header_footer/Header';
 
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="App" style= {{ height: '1500px', background: 'cornflowerblue' }}>
+      <Header />
     </div>
   );
 }
