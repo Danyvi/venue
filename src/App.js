3ff9679396f2853d4447ea4642ce3f0faca7cd23
Daniele Vinci <vinci.dani@gmail.com>
2019-08-26 18:10:47 +0200

    highlight section and button component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Featured from './components/featured/Index';
 import VenueInfo from './components/venueInfo/index';
+import Highlights from './components/Highlights/index';
 
 function App() {
   return (
-    <div className="App" style= {{ height: '1500px', background: 'cornflowerblue' }}>
+    <div className="App" style= {{ height: '1500px' }}>
       <Header />
       <Featured />
       <VenueInfo />
+      <Highlights />
     </div>
   );
 }
---
+++ src/components/Highlights/Description.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+
+export default function Description() {
+  return (
+    <Fade>
+      <div className="center_wrapper">
+        <h2>Highlights</h2>
+        <div className="highlight_description">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, accusantium? Atque doloribus sequi sint fuga inventore porro quibusdam. Ad, voluptates?
+        </div>
+      </div>
+    </Fade>
+  )
+}
