26eeeb169e971a351215e373676856d94bdf2c31
Daniele Vinci <vinci.dani@gmail.com>
2019-08-26 23:44:58 +0200

    scroll

--- src/App.js
+++ src/App.js
@@ -7,16 +7,33 @@ import Highlights from './components/Highlights';
 import Pricing from './components/pricing';
 import Location from './components/location';
 import Footer from './components/header_footer/Footer'
+import {Element} from 'react-scroll';
 
 function App() {
   return (
     <div className="App" style= {{ height: '1500px' }}>
       <Header />
-      <Featured />
-      <VenueInfo />
-      <Highlights />
-      <Pricing />
-      <Location />
+
+      <Element name="featured">
+        <Featured />
+      </Element>
+
+      <Element name="venueinfo">
+        <VenueInfo />
+      </Element>
+
+      <Element name="highlights">
+        <Highlights />
+      </Element>
+
+      <Element name="pricing">
+        <Pricing />
+      </Element>
+
+      <Element name="location">
+        <Location />
+      </Element>
+
       <Footer />
     </div>
   );
